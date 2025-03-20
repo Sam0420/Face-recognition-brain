c6643219d94d5de3379d5ecbaca2ecef9dc5ba6e
sam0420 <samhashimi10@gmail.com>
2025-03-20 11:36:45 +0100

    initialized the core of the project + added a nav bar with a sign out design

---
+++ src/components /Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const Navigation = () => {
+
+    return (
+        <nav style={{display: 'flex' , justifyContent: 'flex-end'}}>
+            <p className="f3 link dim black a3 pointer">Sign out</p>
+        </nav>
+    );
+};
+
+export default Navigation; 
